0b9f48ea39816f33e100d731f5e7c16f5ecaa6d3
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-01-05 21:25:41 -0300

    Create media-aritmetica.py
    Programa que pergunta as notas que o aluno obteve em cada bimestre, calcula a média aritmética e mostra se o aluno está aprovado ou reprovado.

---
+++ media-aritmetica.py
@@ -0,0 +1,10 @@
+n1 = float(input("Nota 1: "))
+n2 = float(input("Nota 2: "))
+n3 = float(input("Nota 3: "))
+n4 = float(input("Nota 4: "))
+media = (n1+n2+n3+n4)/4
+if media >= 6 :
+  print("Aprovado.")
+else :
+  print("Reprovado.")
+  
